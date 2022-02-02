0f8796bb0fb8c9dc311e5ec30a35a6aa7908490b
Riku Esque <rikuesque@Rikus-MacBook-Pro-2.local>
2022-02-02 11:07:35 -0800

    state practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('kanye')
+  const [trueFalse, setTrueFalse] = useState(true)
+
+  const changeName = () => setName('riku')
+  const toggleTrueFalse = () => setTrueFalse(!trueFalse)
+
+  let random = Math.random()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <ul>
+      <li>{random} ← let</li>
+      <li>{name} ← state</li>
+      <li>{trueFalse ? 'true' : 'false'} ← state</li>
+    </ul>
+
+    <button onClick={toggleTrueFalse}>TOGGLE</button>
+
+    <button onClick={changeName}>CHANGE</button>
+  </>
+  )
 }
 
 export default App;
